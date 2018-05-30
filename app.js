73cb328075a972a4ec671a2e93ea3d8ff3204736
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-05-30 01:39:59 -0400

    Adding bpm indicator

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ app.use("/bower",express.static(path.join(__dirname, 'bower_components')));
 //server.listen(app.get('port'), function() {
 //})
 
-var currentdata;
-
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
@@ -54,12 +52,105 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
+var deviationThreshold=20;
+
+var currentData;
+var latestData = [];
+var average;
+var standardDeviation;
+var detectedRise=0;
+var riseTime;
+var heartbeats = [];
+var previousbeat;
+
+var beatsperminute;
+
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('arduino-data', function(data) {
-		currentdata=data
-		//console.log(currentdata);
-		io.emit('update-data',currentdata)
+		currentData=data
+		if(currentData) {
+			latestData.push({time: Date.now(),value: Number(currentData)});
+		}
+		var i;
+		for (i = 0; i < latestData.length; i++) {
+			if (typeof(latestData[i]) == 'undefined') {
+				console.log(currentData)
+			}
+			else if (latestData[i].value > 1000) {
+				console.log(latestData[i].value)
+			}
+			if (latestData[i].time < Date.now()-4000) {
+				latestData.splice(i,1);
+				i--;
+			}
+		}
+		for (i = 0; i < heartbeats.length; i++) {
+			if(heartbeats[i].time < Date.now() - 6000) {
+				heartbeats.splice(i,1);
+				i--;
+			}
+		}
+		while(heartbeats.length>5) {
+			heartBeatTimes=latestData.map(heartbeats => heartbeats.time);
+			i = heartBeatTimes.indexOf(Math.min.apply(null, heartBeatTimes));
+			heartbeats.splice(i,1)
+		}
+		if(heartbeats.length>1) {
+			heartBeatIntervals=heartbeats.map(heartbeats => heartbeats.interval);
+			for (i = 0; i < heartBeatIntervals.length; i++) {
+				if (heartBeatIntervals[i] == NaN) {
+					heartBeatIntervals.splice(i,1);
+				}
+			}
+			if(heartBeatIntervals.length>3) {
+				beatsperminute=60*1000*heartbeats.length/(heartBeatIntervals.reduce(function(acc, val) { return acc + val; }));
+				//console.log(heartBeatIntervals.length);
+				//console.log(heartBeatIntervals);
+				//console.log(heartBeatIntervals.reduce(function(acc, val) { return acc + val; }));
+			}
+			else {
+				beatsperminute=NaN;
+			}
+		}
+		else {
+			beatsperminute=NaN;
+		}
+
+		values = latestData.map(latestData => latestData.value);
+		//console.log(values.reduce(function(acc, val) { return acc + val; }));
+		average = values.reduce(function(acc, val) { return acc + val; })/values.length;
+		standardDeviation = Math.sqrt(values.map(function(value) {
+			return (value - average) * (value - average)
+		}).reduce(function(acc, val) { return acc + val; })/(values.length-1));
+
+		//console.log(typeof(average));
+		//console.log(values);
+		//console.log(values.length + ' , ' + average + ' , ' + standardDeviation);
+		if(detectedRise==0) {
+			if(currentData > average + standardDeviation && standardDeviation > deviationThreshold) {
+				detectedRise=1;
+				riseTime=Date.now();
+				console.log('Rise detected')
+			}
+		}
+		else {
+			if(currentData < average + 0.5*standardDeviation) {
+				if(Date.now() < riseTime + 500) {
+					var intervalTime = Date.now()-previousbeat;
+					if (intervalTime > 10000) {
+						intervalTime = NaN;
+					}
+					heartbeats.push({time: Date.now(), interval: intervalTime});
+					previousbeat=Date.now();
+					console.log(heartbeats);
+				}
+				detectedRise=0;
+			}
+		}
+
+		//console.log(beatsperminute);
+		io.emit('update-data',{sensor: currentData, bpm: beatsperminute})
 	})
 })
 
--- public/javascripts/smoothiehandler.js
+++ public/javascripts/smoothiehandler.js
@@ -1,13 +1,25 @@
-var heartsensordata = new TimeSeries();
+var sensordata = new TimeSeries();
+var beatsperminute = new TimeSeries();
 
 var socket = io('/')
 socket.on('update-data', function(data) {
-	heartsensordata.append(new Date().getTime(),data);
+	sensordata.append(new Date().getTime(),data.sensor);
+	beatsperminute.append(new Date().getTime(),data.bpm);
+	if (data.bpm>25 && data.bpm != NaN) {
+		document.getElementById("bpmindicator").innerHTML = 'Your current heart rate is ' + data.bpm + ' beats per minute.';
+	}
+	else {
+		document.getElementById("bpmindicator").innerHTML = 'The device is disconnected.';
+	}
 	//console.log(data);
 });
 
 function createTimeline() {
-	var chart = new SmoothieChart({responsive: true});
-	chart.addTimeSeries(heartsensordata, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
-	chart.streamTo(document.getElementById("chart"), 500);
+	var sensorchart = new SmoothieChart({responsive: true});
+	sensorchart.addTimeSeries(sensordata, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+	sensorchart.streamTo(document.getElementById("sensorsignal"), 500);
+
+	var bpmchart = new SmoothieChart({responsive: true});
+	bpmchart.addTimeSeries(beatsperminute, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+	bpmchart.streamTo(document.getElementById("bpm"), 500);
 }
